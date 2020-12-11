0859470733eee9cb523cf4145c675be91f358119
VincentEung <73190703+VincentEung@users.noreply.github.com>
2020-12-11 17:33:53 +0100

    Update D_Compress.h

--- D_Compress/D_Compress.h
+++ D_Compress/D_Compress.h
@@ -1,11 +1,27 @@
+/*****************************************************************//**
+ * \file   DoublyLinkedList.h
+ * \brief  Function used to compress files and create a new file
+ * 
+ * \author VincentEung and Ralys Lallali
+ * \date   11 December 2020
+ *********************************************************************/
+
 #ifndef D_COMPRESS_H_INCLUDED
 #define D_COMPRESS_H_INCLUDED
 
 
-
 void creationFichierCodage();
+
+/**
+ * \brief Function to create a new text file in which we put the caracters we find 
+ */
+
 void decodage();
 
-
+/**
+ * \brief Function text translated in binary
+ * \get back the code in the files
+ * \pointeurDicoCode is the pointer which scroll the dico until it finds the right value 
+ */
 
 #endif // D_COMPRESS_H_INCLUDED
